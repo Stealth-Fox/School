80a97bce725ec46a260c518d2b522ef2489489e3
Stealth-Fox <sergio_suteu@yahoo.it>
2018-11-06 11:10:36 +0100

    Primo commit del programma

---
+++ MaxMinFinder/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace MaxMinFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numBox1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numBox2 = new System.Windows.Forms.NumericUpDown();
+            this.numBox3 = new System.Windows.Forms.NumericUpDown();
+            this.numBox4 = new System.Windows.Forms.NumericUpDown();
+            this.numBox5 = new System.Windows.Forms.NumericUpDown();
+            this.numBox6 = new System.Windows.Forms.NumericUpDown();
+            this.numBox7 = new System.Windows.Forms.NumericUpDown();
+            this.numBox8 = new System.Windows.Forms.NumericUpDown();
+            this.numBox9 = new System.Windows.Forms.NumericUpDown();
+            this.numBox10 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minBox = new System.Windows.Forms.TextBox();
+            this.maxBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.orderBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numBox1
+            // 
+            this.numBox1.Location = new System.Drawing.Point(15, 25);
+            this.numBox1.Name = "numBox1";
+            this.numBox1.Size = new System.Drawing.Size(50, 20);
+            this.numBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numeri";
+            // 
+            // numBox2
+            // 
+            this.numBox2.Location = new System.Drawing.Point(71, 25);
+            this.numBox2.Name = "numBox2";
+            this.numBox2.Size = new System.Drawing.Size(50, 20);
+            this.numBox2.TabIndex = 2;
+            // 
+            // numBox3
+            // 
+            this.numBox3.Location = new System.Drawing.Point(127, 25);
+            this.numBox3.Name = "numBox3";
+            this.numBox3.Size = new System.Drawing.Size(50, 20);
+            this.numBox3.TabIndex = 3;
+            // 
+            // numBox4
+            // 
+            this.numBox4.Location = new System.Drawing.Point(183, 25);
+            this.numBox4.Name = "numBox4";
+            this.numBox4.Size = new System.Drawing.Size(50, 20);
+            this.numBox4.TabIndex = 4;
+            // 
+            // numBox5
+            // 
+            this.numBox5.Location = new System.Drawing.Point(239, 25);
+            this.numBox5.Name = "numBox5";
+            this.numBox5.Size = new System.Drawing.Size(50, 20);
+            this.numBox5.TabIndex = 5;
+            // 
+            // numBox6
+            // 
+            this.numBox6.Location = new System.Drawing.Point(15, 51);
+            this.numBox6.Name = "numBox6";
+            this.numBox6.Size = new System.Drawing.Size(50, 20);
+            this.numBox6.TabIndex = 6;
+            // 
+            // numBox7
+            // 
+            this.numBox7.Location = new System.Drawing.Point(71, 51);
+            this.numBox7.Name = "numBox7";
+            this.numBox7.Size = new System.Drawing.Size(50, 20);
+            this.numBox7.TabIndex = 7;
+            // 
+            // numBox8
+            // 
+            this.numBox8.Location = new System.Drawing.Point(127, 51);
+            this.numBox8.Name = "numBox8";
+            this.numBox8.Size = new System.Drawing.Size(50, 20);
+            this.numBox8.TabIndex = 8;
+            // 
+            // numBox9
+            // 
+            this.numBox9.Location = new System.Drawing.Point(183, 51);
+            this.numBox9.Name = "numBox9";
+            this.numBox9.Size = new System.Drawing.Size(50, 20);
+            this.numBox9.TabIndex = 9;
+            // 
+            // numBox10
+            // 
+            this.numBox10.Location = new System.Drawing.Point(239, 51);
+            this.numBox10.Name = "numBox10";
+            this.numBox10.Size = new System.Drawing.Size(50, 20);
+            this.numBox10.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Minimo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Massimo";
+            // 
+            // minBox
+            // 
+            this.minBox.Location = new System.Drawing.Point(66, 90);
+            this.minBox.Name = "minBox";
+            this.minBox.Size = new System.Drawing.Size(100, 20);
+            this.minBox.TabIndex = 13;
+            // 
+            // maxBox
+            // 
+            this.maxBox.Location = new System.Drawing.Point(66, 116);
+            this.maxBox.Name = "maxBox";
+            this.maxBox.Size = new System.Drawing.Size(100, 20);
+            this.maxBox.TabIndex = 14;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(197, 90);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 46);
+            this.SearchButton.TabIndex = 15;
+            this.SearchButton.Text = "Cerca";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // orderBox
+            // 
+            this.orderBox.Location = new System.Drawing.Point(66, 142);
+            this.orderBox.Name = "orderBox";
+            this.orderBox.Size = new System.Drawing.Size(206, 20);
+            this.orderBox.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Ordinato";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 172);
+            this.Controls.Add(this.orderBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.maxBox);
+            this.Controls.Add(this.minBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numBox10);
+            this.Controls.Add(this.numBox9);
+            this.Controls.Add(this.numBox8);
+            this.Controls.Add(this.numBox7);
+            this.Controls.Add(this.numBox6);
+            this.Controls.Add(this.numBox5);
+            this.Controls.Add(this.numBox4);
+            this.Controls.Add(this.numBox3);
+            this.Controls.Add(this.numBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numBox2;
+        private System.Windows.Forms.NumericUpDown numBox3;
+        private System.Windows.Forms.NumericUpDown numBox4;
+        private System.Windows.Forms.NumericUpDown numBox5;
+        private System.Windows.Forms.NumericUpDown numBox6;
+        private System.Windows.Forms.NumericUpDown numBox7;
+        private System.Windows.Forms.NumericUpDown numBox8;
+        private System.Windows.Forms.NumericUpDown numBox9;
+        private System.Windows.Forms.NumericUpDown numBox10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox minBox;
+        private System.Windows.Forms.TextBox maxBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox orderBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MaxMinFinder/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxMinFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            decimal[] numberList = new decimal[10];
+            decimal temp = 0;
+            decimal max = 0;
+            decimal min = 0;
+            orderBox.Text = "";
+
+            numberList[0] = numBox1.Value;
+            numberList[1] = numBox2.Value;
+            numberList[2] = numBox3.Value;
+            numberList[3] = numBox4.Value;
+            numberList[4] = numBox5.Value;
+            numberList[5] = numBox6.Value;
+            numberList[6] = numBox7.Value;
+            numberList[7] = numBox8.Value;
+            numberList[8] = numBox9.Value;
+            numberList[9] = numBox10.Value;
+
+            min = numberList[0];
+
+            for (int i=0; i<10; i++)
+            {
+                temp = numberList[i];
+                if (temp > max) { max = temp; }
+                if (temp < min) { min = temp; }
+            }
+
+            maxBox.Text = max.ToString();
+            minBox.Text = min.ToString();
+
+            Array.Sort(numberList);
+
+            for (int i = 0; i < 10; i++)
+            {
+                orderBox.Text = orderBox.Text + " " + numberList[i].ToString() + " /";
+            }
+        }
+    }
+}
